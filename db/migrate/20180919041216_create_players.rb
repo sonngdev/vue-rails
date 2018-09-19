12919c2a8b432e0f784b6bafb7d9113a05efe8b5
nguyenthanhson <son.thanh.97@gmail.com>
2018-09-19 11:23:22 +0700

    Created models and controllers for teams and players

---
+++ db/migrate/20180919041216_create_players.rb
@@ -0,0 +1,13 @@
+class CreatePlayers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :players do |t|
+      t.integer :team_id
+      t.string :name
+      t.string :position
+
+      t.timestamps
+    end
+
+    add_index :players, :team_id
+  end
+end
