34020b278085e98dd8dcfd20a9e834417586aeb5
nguyenthanhson <son.thanh.97@gmail.com>
2018-09-21 09:45:54 +0700

    Install webpacker and support Vue

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -69,6 +69,6 @@ class TeamsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def team_params
-      params.require(:team).permit(:name)
+      params.require(:team).permit(:name, players_attributes: [:id, :name, :position, :_destroy])
     end
 end
