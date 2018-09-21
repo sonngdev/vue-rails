66a24c5c9f993c2694a69f2c594256a805ba9fa5
nguyenthanhson <son.thanh.97@gmail.com>
2018-09-21 22:32:28 +0700

    finish part 1

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -5,17 +5,43 @@
 // like app/views/layouts/application.html.erb.
 // All it does is render <div>Hello Vue</div> at the bottom of the page.
 
-import Vue from 'vue'
-import App from '../app.vue'
+import Vue from "vue/dist/vue.esm"
+import TurbolinksAdapter from "vue-turbolinks"
+import VueResource from "vue-resource"
 
-document.addEventListener('DOMContentLoaded', () => {
-  const el = document.body.appendChild(document.createElement('hello'))
-  const app = new Vue({
-    el,
-    render: h => h(App)
-  })
+Vue.use(VueResource);
+Vue.use(TurbolinksAdapter);
 
-  console.log(app)
+document.addEventListener("turbolinks:load", () => {
+  Vue.http.headers.common["X-CSRF-Token"] = document.querySelector("meta[name='csrf-token']").getAttribute("content")
+
+  const element = document.getElementById("team-form");
+  if (element != null) {
+    let team = JSON.parse(element.dataset.team);
+    let players_attributes = JSON.parse(element.dataset.playersAttributes);
+    players_attributes.forEach(player => player._destroy = null);
+    team.players_attributes = players_attributes;
+
+    const app = new Vue({
+        el: element,
+        data: function() {
+          return { team }
+        },
+        methods: {
+          addPlayer() {
+            team.players_attributes.push({
+              id: null,
+              name: "",
+              position: "",
+              _destroy: null
+            })
+          },
+          removePlayer(index) {
+            this.team.players_attributes.splice(index, 1);
+          }
+        }
+    })
+  }
 })
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_19_041216) do
+ActiveRecord::Schema.define(version: 2018_09_21_022756) do
 
   create_table "players", force: :cascade do |t|
     t.integer "team_id"
@@ -18,7 +18,6 @@ ActiveRecord::Schema.define(version: 2018_09_19_041216) do
     t.string "position"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["team_id"], name: "index_players_on_team_id"
   end
 
   create_table "teams", force: :cascade do |t|
