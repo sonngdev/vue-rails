2fc5797bcba661fcac05beb2a5090ed68ce4862e
nguyenthanhson <son.thanh.97@gmail.com>
2018-09-25 00:29:31 +0700

    include dropdown

---
+++ app/javascript/components/dropdown.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="relative inline-block">
+    <div role="button" class="inline-block select-none" @click.prevent="open = !open">
+      <slot name="link"></slot>
+    </div>
+
+    <div v-show="open" class="absolute pin-r w-32 mt-2 z-10">
+      <slot name="dropdown-items"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        open: false
+      }
+    }
+  }
+</script>
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,17 +1,20 @@
 /* eslint no-console:0 */
 // This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
+// present in this directory. You"re encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
+// that code so it"ll be compiled.
 //
-// To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
+// To reference this file, add <%= javascript_pack_tag "application" %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-import Vue from 'vue/dist/vue.esm'
-import TurbolinksAdapter from 'vue-turbolinks'
+import Vue from "vue/dist/vue.esm"
+import TurbolinksAdapter from "vue-turbolinks"
 Vue.use(TurbolinksAdapter)
 
-document.addEventListener('turbolinks:load', () => {
+import Dropdown from "../components/dropdown"
+Vue.component("dropdown", Dropdown)
+
+document.addEventListener("turbolinks:load", () => {
   const app = new Vue({
     el: "[data-behavior='vue']"
   })
