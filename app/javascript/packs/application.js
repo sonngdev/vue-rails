2fc3baa3a371717ce5256bd29e3176665bb417a8
nguyenthanhson <son.thanh.97@gmail.com>
2018-09-24 23:49:43 +0700

    navbar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,4 +7,12 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-console.log('Hello World from Webpacker')
+import Vue from 'vue/dist/vue.esm'
+import TurbolinksAdapter from 'vue-turbolinks'
+Vue.use(TurbolinksAdapter)
+
+document.addEventListener('turbolinks:load', () => {
+  const app = new Vue({
+    el: "[data-behavior='vue']"
+  })
+})
